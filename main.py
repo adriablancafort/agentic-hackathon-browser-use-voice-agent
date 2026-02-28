b4d190a0e8c41cf149cf0efa62328384efc49f09
Adrià <adriablancafort@gmail.com>
2026-02-28 13:21:09 +0100

    Browseruse init

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-def main():
-    print("Hello from agentic-hackathon!")
+import asyncio
+
+from browser_use import Agent, ChatBrowserUse
+from dotenv import load_dotenv
+
+
+async def main() -> None:
+    load_dotenv()
+    llm = ChatBrowserUse()
+    task = "Find the number 1 post on Show HN"
+    agent = Agent(task=task, llm=llm)
+    await agent.run()
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
